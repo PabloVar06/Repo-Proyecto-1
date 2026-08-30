ef7da5422eb1338219daa31537249ea2576bb205
CapiTakai <pablitoevm@hotmail.com>
2026-08-30 18:13:28 -0500

    docs:JS correcciones

---
+++ Version-JavaScript/src/catalogo.js
@@ -0,0 +1,10 @@
+// Se establece las listas de las categorias plataforma y generos
+export const catalogoJuegos = [
+    {nombre: "Marvel's Spider-Man", genero: "Accion", plataforma: "PlayStation", fechaLanzamiento: new Date("2018-09-07"), puntaje: 87},
+    {nombre: "The Legend of Zelda: Breath of the Wild", genero: "Aventura", plataforma: "Nintendo Switch", fechaLanzamiento: new Date("2017-03-03"), puntaje: 97},
+    {nombre: "FIFA 21", genero: "Deportes", plataforma: "PC", fechaLanzamiento: new Date("2020-10-09"), puntaje: 85},
+    {nombre: "The Witcher 3: Wild Hunt", genero: "Rol", plataforma: "PC", fechaLanzamiento: new Date("2015-05-19"), puntaje: 93},
+    {nombre: "Resident Evil 7: Biohazard", genero: "Terror", plataforma: "PC", fechaLanzamiento: new Date("2017-01-24"), puntaje: 89},
+    {nombre: "Mario Kart 8 Deluxe", genero: "Carreras", plataforma: "Nintendo Switch", fechaLanzamiento: new Date("2017-04-28"), puntaje: 92},
+    {nombre: "Among Us", genero: "Otros", plataforma: "PC", fechaLanzamiento: new Date("2018-06-15"), puntaje: 84}
+]
